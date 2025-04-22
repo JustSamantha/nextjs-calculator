5751d12f82b6345a49c1ba375c93c5a6d2cc14b7
Samantha Castillo <samanthacas@pm.me>
2025-04-22 10:44:24 -0700

    - Adds logic for the calculator - Adds html structure and styling - Adds documentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,101 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from 'react';
+
+let currentOperator:string = '';
+let previousDisplay:string = '';
+let clearDisplayBeforeNumber:boolean = false;
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const calculatorInputSymbols:Array<string> = ['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '/', '.', '0', '=', '*', 'C'];
+  const [display, setDisplay] = useState('');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  /**
+   * Receives the user input and does the right action based on it
+   * @param {string} symbol The symbol that the user clicked on
+   */
+  function addInput(symbol:string) {
+    if (isOperatorSymbol(symbol)) {
+      setOperator(symbol);
+    } else if (symbol === '=') {
+      doMath();
+    } else if (symbol === 'C') {
+      clearCalculator();
+    } else {
+      addNumber(symbol);
+    }
+  }
+
+  /**
+   * Adds a number to the display and clears the interface when needed
+   * @param {string} number The number to add to the display
+   */
+  function addNumber(number:string) {
+    if (clearDisplayBeforeNumber) {
+      previousDisplay = display;
+      clearDisplayBeforeNumber = false;
+      setDisplay(number);
+    } else {
+      setDisplay(display+number);
+    }
+  }
+
+  /**
+   * Sets the math operator that the user clicked
+   * @param {string} operator The math operator to process
+   */
+  function setOperator(operator:string) {
+    if (currentOperator === '') {
+      currentOperator = operator;
+      previousDisplay = display;
+      clearDisplayBeforeNumber = true;
+    } else {
+      doMath();
+    }
+  }
+
+  /**
+   * Asserts if the provided symbol is a supported math operator
+   * @param {string} symbol The symbol to assert
+   * @returns {boolean} True if it's a supported math operator
+   */
+  function isOperatorSymbol(symbol:string) {
+    const operatorSymbols:Array<string> = ['+', '-', '/', '*'];
+    return !!operatorSymbols.find((currentSymbol) => currentSymbol===symbol);
+  }
+  
+  /**
+   * Crunches the calculator numbers based on previously provided data
+   */
+  function doMath() {
+    if (previousDisplay && currentOperator && display) {
+      setDisplay(eval(previousDisplay+currentOperator+display));
+      currentOperator = '';
+      previousDisplay = '';
+      clearDisplayBeforeNumber = true;
+    }
+  }
+
+  /**
+   * Resets the calculator to its inital state
+   */
+  function clearCalculator() {
+    setDisplay('');
+    currentOperator = '';
+    previousDisplay = '';
+  }
+
+  return (
+    <div className="flex h-screen justify-center mt-[50px]">
+      <div>
+        <input className="border-1 w-full rounded text-right pr-[5px] pl-[5px] h-[50px]" type="text" value={display} disabled />
+        <br />
+        <div className="w-[200px] grid grid-cols-4 grid-rows-5">
+          {calculatorInputSymbols.map((symbol) => (
+            <button className="hover:bg-pink-200 border-1 rounded h-[30px]" key={symbol} onClick={() => addInput(symbol)}>{symbol}</button>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
